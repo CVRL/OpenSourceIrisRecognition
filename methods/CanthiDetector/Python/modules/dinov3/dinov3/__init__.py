e63644e092bed800e57bb97546eb08ad7b51c4dc
Rasel Ahmed Bhuiyan <rbhuiyan@nd.edu>
2026-05-26 15:20:04 -0400

    removed canthi detector

--- methods/CanthiDetector/Python/modules/dinov3/dinov3/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-#
-# This software may be used and distributed in accordance with
-# the terms of the DINOv3 License Agreement.
-
-__version__ = "0.0.1"
--- methods/CanthiDetector/Python/modules/dinov3/dinov3/loss/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-#
-# This software may be used and distributed in accordance with
-# the terms of the DINOv3 License Agreement.
-
-from .dino_clstoken_loss import DINOLoss
-from .gram_loss import GramLoss
-from .ibot_patch_loss import iBOTPatchLoss
-from .koleo_loss import KoLeoLoss, KoLeoLossDistributed
